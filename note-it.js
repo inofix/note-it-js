81df3cc7446199b86df91eb8aeb7e880a126be89
Michael Lustenberger <mic@inofix.ch>
2018-04-26 17:06:29 +0200

    Multi Color
    Add another color stack.

--- note-it.js
+++ note-it.js
@@ -115,19 +115,34 @@ function create_noteit(color, text) {
         n.raise();
     }));
 //    noteits.push(n);
+    return n;
 };
 
-var stock = draw_noteit();
-stock.append("text").text("+").attr("x", "9em").attr("y", "1em");
-stock.on("click", function() { create_noteit(null, "...") } );
+var rstock = draw_noteit("#ff6ee2");
+rstock.append("text").text("+").attr("x", "9em").attr("y", "1em");
+rstock.on("click", function() {
+    let n = create_noteit("#ff6ee2", "...");
+    n.attr("transform", "translate(" + d3.event.x + "," + d3.event.y + ")");
+} );
 
-var bstock = draw_noteit("#ff3333");
+var bstock = draw_noteit("#6ee0ff");
 bstock.attr("transform", "translate(26,26)");
 bstock.append("text").text("+").attr("x", "9em").attr("y", "1em");
-bstock.on("click", function() { create_noteit("#ff3333", "...") } );
+bstock.on("click", function() {
+    let n = create_noteit("#6ee0ff", "...");
+    n.attr("transform", "translate(" + d3.event.x + "," + d3.event.y + ")");
+} );
 
+var ystock = draw_noteit();
+ystock.attr("transform", "translate(52,52)");
+ystock.append("text").text("+").attr("x", "9em").attr("y", "1em");
+ystock.on("click", function() {
+    let n = create_noteit(null, "...");
+    n.attr("transform", "translate(" + d3.event.x + "," + d3.event.y + ")");
+} );
 
 // TODO - add a note-it dropped on another to the lower ones group
+// TODO - colapse / expand all children on double-click
 // TODO - add a textbox for content / functions below the title (9em remaining)
 // TODO - display the config as json
 // TODO - add sync between json:svg
