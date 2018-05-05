70a847f4d58e9b474bd6cb7caa030a7fe5cb32fd
Michael Lustenberger <mic@inofix.ch>
2018-05-05 02:12:21 +0200

    Refactoring
    Start all over. More modular, less code. But still it does not work in
    FF..

--- init.js
+++ init.js
@@ -1,27 +1,10 @@
 var svg = d3.select("svg");
 
-var rstock = draw_noteit("#ff6ee2");
-rstock.append("text").text("+").attr("x", "9em").attr("y", "1em");
-rstock.on("click", function() {
-    let n = create_noteit("#ff6ee2", "...");
-    n.attr("transform", "translate(" + d3.event.x + "," + d3.event.y + ")");
-} );
+var rstock = create_stock("Type", "#ff6ee2");
 
-var bstock = draw_noteit("#6ee0ff");
-bstock.attr("transform", "translate(26,26)");
-bstock.append("text").text("+").attr("x", "9em").attr("y", "1em");
-bstock.on("click", function() {
-    let n = create_noteit("#6ee0ff", "...");
-    n.attr("transform", "translate(" + d3.event.x + "," + d3.event.y + ")");
-} );
+var bstock = create_stock("Type", "#6ee0ff", 26, 26);
 
-var ystock = draw_noteit();
-ystock.attr("transform", "translate(52,52)");
-ystock.append("text").text("+").attr("x", "9em").attr("y", "1em");
-ystock.on("click", function() {
-    let n = create_noteit(null, "...");
-    n.attr("transform", "translate(" + d3.event.x + "," + d3.event.y + ")");
-} );
+var ystock = create_stock("Type", "", 52, 52);
 
 // TODO - add a note-it dropped on another to the lower ones group
 // TODO - colapse / expand all children on double-click
