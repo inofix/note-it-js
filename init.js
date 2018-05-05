8cb20f1c083873c6eef6b32106649547ee619d3f
Michael Lustenberger <mic@inofix.ch>
2018-05-05 13:59:44 +0200

    Minor Changes
    Move stacks closer together..

--- init.js
+++ init.js
@@ -1,10 +1,8 @@
 var svg = d3.select("svg");
 
 var rstock = create_stock("Type", "#ff6ee2");
-
-var bstock = create_stock("Type", "#6ee0ff", 26, 26);
-
-var ystock = create_stock("Type", "", 52, 52);
+var bstock = create_stock("Type", "#6ee0ff", 8, 26);
+var ystock = create_stock("Type", "", 16, 52);
 
 // TODO - add a note-it dropped on another to the lower ones group
 // TODO - colapse / expand all children on double-click
