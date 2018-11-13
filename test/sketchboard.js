9f30c96ea8803ba1cb3bba28b0e21ac594f6f906
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-11-13 16:32:30 +0100

    add basic ava setup for test

---
+++ test/sketchboard.js
@@ -0,0 +1,14 @@
+import test from "ava";
+import SketchBoard from "../lib/sketchboard";
+
+test.before(t => {
+  const s = document.createElement("div");
+  s.setAttribute("id", "sketchboard");
+  document.body.appendChild(s);
+});
+
+test("it creates a sketchboard with an id from a DOM selector", t => {
+  const board = new SketchBoard("#sketchboard");
+  const uid = board.getUid();
+  t.truthy(uid);
+});
